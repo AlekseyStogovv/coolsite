f08fb28d7bd5f3dc8dc371fac42ce204a84a8303
AlekseyStogovv <astogovv@gmail.com>
2023-08-14 17:13:31 +0300

    добавлена регистрация

--- women/views.py
+++ women/views.py
@@ -1,3 +1,4 @@
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseNotFound
@@ -112,3 +113,12 @@ class ShowPost(DataMixin, DetailView):
 #     }
 #     return render(request, 'women/post.html', context=context)
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
